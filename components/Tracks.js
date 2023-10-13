dcff81020b6b77d9872cdcfe8e889f33a4fdae21
Raj Patel <riskyrick20@gmail.com>
2023-10-13 20:19:08 +0530

    removed tracks and added the themes

--- components/Tracks.js
+++ components/Tracks.js
@@ -11,7 +11,7 @@ import {
 export default function Tracks() {
   return (
     <Box marginBlock={16} id="tracks">
-      <Heading marginBlock={"8"}>Tracks</Heading>
+      <Heading marginBlock={"8"}>Major themes</Heading>
       <Grid
         gridAutoRows="1fr"
         templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
@@ -20,50 +20,14 @@ export default function Tracks() {
       >
         <GridItem p="4">
           <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/fd25dfdc-c699-4e4b-946c-459405520d52"
-            trackName="Streamlined Tax Filing for Freelancers"
+            imageUrl="https://image.lexica.art/full_jpg/0d585bf4-aa92-4864-8ea6-20f2a17a6e70"
+            trackName="Taxation"
           />
         </GridItem>
         <GridItem p="4">
           <TrackCard
             imageUrl="https://image.lexica.art/full_jpg/58e2902c-29dd-49ce-9149-3c4975b9d468"
-            trackName="Tax Filing Assistance for Small Businesses"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://image.lexica.art/full_jpg/be1f98f4-31b0-4d8b-b13b-2ed3d4d8f001"
-            trackName="Personalized Tax Optimization"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://image.lexica.art/full_jpg/c45219aa-514a-4a2a-9cb8-0c79f59a584b"
-            trackName="Gamified Tax Education"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/a2c3ab34-800c-492b-aee7-836992478ec8"
-            trackName="Financial Statement Generation"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/d8d45c0e-226c-4d44-a91a-614e92b88240"
-            trackName="Financial Health Assessment"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/4bf57a24-6927-44c6-a63c-b01e524a055e"
-            trackName="Comparative Analysis"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://image.lexica.art/full_jpg/6bf112cf-e487-46b5-aba9-b3f2249e1be9"
-            trackName="User-Friendly Interface"
+            trackName="Financial Analysis of Unlisted Companies"
           />
         </GridItem>
       </Grid>
